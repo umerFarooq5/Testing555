de7569099fd9d8c4f237882a2ce7c233d195af98
umer malik <umer.0malik@gmail.com>
2022-01-17 21:55:47 +0000

    branch commit

---
+++ Test/SecondView.swift
@@ -0,0 +1,20 @@
+//
+//  SecondView.swift
+//  Test
+//
+//  Created by umer malik on 17/01/2022.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text("World")
+    }
+}
+
+struct SecondView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondView()
+    }
+}
