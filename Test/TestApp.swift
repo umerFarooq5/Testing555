85adf3068ee733bc3e023795859dbb099f7d4ae5
umer malik <umer.0malik@gmail.com>
2022-01-17 21:48:08 +0000

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by umer malik on 17/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
